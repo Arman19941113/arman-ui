75cf604c19f04e2be3f35a9e2a6f07f56af05a3b
Arman19941113 <td19941113@gmail.com>
2020-06-03 19:57:30 +0800

    example add search feature

--- example/views/AppHeader.vue
+++ example/views/AppHeader.vue
@@ -14,33 +14,93 @@
                 <router-link class="nav-item" to="/guides">指南</router-link>
                 <router-link class="nav-item" to="/components">组件</router-link>
             </div>
-            <!-- todo -->
-            <AInput round placeholder="搜索" @input="handleInput" @blur="handleBlur"/>
+            <AInput ref="reference" round placeholder="搜索"
+                v-model="searchValue"
+                @update:modelValue="handleInputUpdate"
+                @click.stop
+                @focus="handleFocus" />
+            <APopper :value="Boolean(searchValue && showPopper)" :reference="reference">
+                <div class="search-result">
+                    <div v-if="Boolean(searchValue && !matchedNames.length)" class="empty-search">没有找到匹配文档</div>
+                    <ul v-else class="match-list">
+                        <li class="match-item" v-for="name in matchedNames" @click="goToSearchItem(name)">{{name}}</li>
+                    </ul>
+                </div>
+            </APopper>
         </div>
     </header>
 </template>
 
 <script>
+    import { ref } from 'vue'
     import AIcon from '@/components/icon'
     import AInput from '@/components/input'
+    import APopper from '@/components/popper'
     import { router } from '../router'
+    import guidesGroups from '../guides'
+    import componentsGroups from '../components'
 
     export default {
         name: 'AppHeader',
-        components: { AIcon, AInput },
-        methods: {
-            handleClickLogo () {
-                router.push({
-                    name: 'home',
-                })
-            },
-            handleInput (e) {
-                console.log(e.target.value)
-            },
-            handleBlur (e) {
-                console.log(e.target.value)
+        components: { AIcon, AInput, APopper },
+        setup () {
+            const reference = ref(null)
+            const { showPopper, handleFocus } = useClickSearch()
+            const { handleInputUpdate, searchValue, matchedNames } = useInputSearch()
+
+            return {
+                reference,
+                showPopper, handleFocus,
+                searchValue, matchedNames, handleInputUpdate,
+                handleClickLogo () {
+                    router.push({ name: 'home' })
+                },
+                goToSearchItem (name) {
+                    router.push({ name: name.toLowerCase().split(' ')[0] })
+                    showPopper.value = false
+                },
             }
         },
+    }
+
+    function useClickSearch () {
+        let showPopper = ref(false)
+        const handleFocus = () => {
+            showPopper.value = true
+            document.addEventListener('click', () => {
+                showPopper.value = false
+            }, { once: true })
+        }
+
+        return { showPopper, handleFocus }
+    }
+
+    function useInputSearch () {
+        const searchValue = ref('')
+        const names = [...getNames(guidesGroups), ...getNames(componentsGroups)]
+        const matchedNames = ref([])
+        const handleInputUpdate = value => {
+            matchedNames.value.splice(0)
+            value !== '' && names.forEach(name => {
+                if (name.toLowerCase().includes(value.toLowerCase())) {
+                    matchedNames.value.push(name)
+                }
+            })
+        }
+
+        return {
+            handleInputUpdate, searchValue, matchedNames,
+        }
+
+        function getNames (groups) {
+            const names = []
+            groups.forEach(group => {
+                group.components.forEach(component => {
+                    names.push(component.name)
+                })
+            })
+            return names
+        }
     }
 </script>
 
@@ -117,6 +177,29 @@
                         &::after {
                             transform: scaleX(.7);
                             transition: all .3s;
+                        }
+                    }
+                }
+            }
+            .search-result {
+                width: 240px;
+                padding: 10px 0;
+                border: 1px solid $borderColor;
+                border-radius: 2px;
+                background-color: $whiteColor;
+                font-size: 12px;
+                line-height: 36px;
+                .empty-search {
+                    padding: 0 12px;
+                }
+                .match-list {
+                    .match-item {
+                        padding: 0 12px;
+                        transition: background-color .2s;
+                        cursor: pointer;
+                        &:hover {
+                            background-color: $cursorBlueColor;
+                            transition: background-color .2s;
                         }
                     }
                 }
