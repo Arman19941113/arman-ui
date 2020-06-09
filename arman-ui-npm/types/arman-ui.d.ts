d25f61ac0ea7f5b6a6ae63790aab6e5a5fd28bea
Arman19941113 <td19941113@gmail.com>
2020-06-09 10:12:15 +0800

    update types

--- arman-ui-npm/types/arman-ui.d.ts
+++ arman-ui-npm/types/arman-ui.d.ts
@@ -1,7 +1,12 @@
+import { Icon } from './components/icon'
 import { Button } from './components/button'
+import { Input } from './components/input'
+import { ColorPicker } from './components/color-picker'
 import { Loading } from './components/loading'
+import { Popper } from './components/popper'
 
 export {
-    Button,
-    Loading,
+    Icon, Button,
+    Input, ColorPicker,
+    Loading, Popper,
 }
